4a5b3132523710d13cee8b9d55fe3c100d4368b1
AndySama79 <arnab.dey@plaksha.edu.in>
2022-12-01 00:35:40 +0530

    simple calculator p10.py

--- p10.py
+++ p10.py
@@ -1,9 +1,4 @@
-import sys
-
-arg = sys.argv
-
-
-def calculate(op1, op2, op):
+def operate(op1, op2, op):
     if op == '+':
         return op1 + op2
     elif op == '-':
@@ -12,3 +7,32 @@ def calculate(op1, op2, op):
         return op1 * op2
     elif op == '/':
         return int(op1 / op2)
+def calculate(*args):
+    num = []
+    op = []
+    for element in args:
+        if element == '+' or element == '-' or element == '/' or element == '*':
+            op.append(element)
+        else:
+            num.append(element)
+    
+    while len(op) != 0 and len(num) >= 2:
+        n1 = num[0]
+        n2 = num[1]
+        ope = op[0]
+        res = operate(n1, n2, ope)
+        if len(num) != 2:
+            num = [res] + num[2:]
+        else:
+            num = [res]
+        if len(op) != 1:
+            op = op[1:]
+        else:
+            op = []
+    
+    return num[0]
+
+#print(calculate(2, '+', 3, '*', 5, '-', 3, '*', 4, '+', -6, '/', 11))
+print(calculate(2, "+", 3, "*", 5, "-", 3, '*', 4, '+', -6, '/', 11))
+
+
