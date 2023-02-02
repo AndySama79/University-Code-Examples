85dc5413222a76aa6f9b22772bcbab964d9a2d2d
AndySama79 <arnab.dey@plaksha.edu.in>
2023-02-02 19:57:01 +0530

    hmm2

--- W02_P2/W02_P2.py
+++ W02_P2/W02_P2.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #%% read data
 data = pd.read_csv("data/netflix_titles.csv")
@@ -33,7 +34,23 @@ plt.savefig("Question2.png")
 plt.show()
 
 # %%    Question 3
+data["date_added"] = pd.to_datetime(data["date_added"])
+data["year"] = data["date_added"].dt.year#.astype(dtype="int")
+data["month"] = data["date_added"].dt.month#.astype(dtype="int")
+
+movies = data[data["type"] == "Movie"]
+shows = data[data["type"] == "TV Show"]
+
+movie_pivot = pd.pivot_table(movies, index=["month"], columns=["year"], aggfunc='count')["date_added"]
+show_pivot = pd.pivot_table(shows, index=["month"], columns=["year"], aggfunc='count')["date_added"]
+
+movie_heat = sns.heatmap(movie_pivot).set_title("Movie Heatmap")
+plt.show()
+
+show_heat = sns.heatmap(show_pivot).set_title("TV Show Heatmap")
+plt.show()
 
 # %%    Question 4
+
 
 # %%    Question 5
